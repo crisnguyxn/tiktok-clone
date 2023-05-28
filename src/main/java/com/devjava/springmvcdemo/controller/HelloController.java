38fc6173b9e65305362de780b4c001290e99ea2e
crisnguyxn <hyundev1406@gmail.com>
2023-05-28 15:28:58 +0700

    initial-app

---
+++ src/main/java/com/devjava/springmvcdemo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.devjava.springmvcdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+    @RequestMapping(value="/",method = RequestMethod.GET)
+    public String getHomepage() {
+        return "home";
+    }
+}
